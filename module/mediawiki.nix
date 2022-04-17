05939904ce02d7f65652d626cc8a2c72c1ec1167
Asmund Berge <asmund@berge.id>
2022-04-17 22:10:16 +0200

    Mediawiki: Make wiki private

--- module/mediawiki.nix
+++ module/mediawiki.nix
@@ -26,6 +26,15 @@
     extraConfig = ''
       $wgLanguageCode = 'no';
       $wgNamespacesWithSubpages[NS_MAIN] = 1;
+
+      # Disable reading by anonymous users
+      $wgGroupPermissions['*']['read'] = false;
+      
+      # Disable anonymous editing
+      $wgGroupPermissions['*']['edit'] = false;
+      
+      # Prevent new user registrations except by sysops
+      $wgGroupPermissions['*']['createaccount'] = false;
       '';
   };
 
