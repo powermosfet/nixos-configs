a2009fb02c36d5cc834293126f5a7792b57136fc
Asmund Berge <asmund@berge.id>
2022-04-17 15:10:55 +0200

    ALS: Don't import secrets

--- module/als/default.nix
+++ module/als/default.nix
@@ -1,9 +1,9 @@
 { config, pkgs, ... }:
 
 let
-  secrets = import /run/keys/als-secrets.nix;
   port = 8002;
-
+  clientId = "029cc178-e604-4d7f-a17c-a41f92c3182c";
+  listId = "AQMkADAwATNiZmYAZC0xZjgwLTE2YzUtMDACLTAwCgAuAAADQGkXSjdFSkyRkPd2Ueax6wEA-3jtJitJ3EmtwtXWE62J7QAByGcE3wAAAA==";
 in
 {
   imports =
@@ -13,10 +13,8 @@ in
   services.als = {
     enable = true;
     port = port;
-    clientId = "029cc178-e604-4d7f-a17c-a41f92c3182c";
-    accessToken = alsSecrets.accessToken;
-    refreshToken = alsSecrets.refreshToken;
-    listId = "AQMkADAwATNiZmYAZC0xZjgwLTE2YzUtMDACLTAwCgAuAAADQGkXSjdFSkyRkPd2Ueax6wEA-3jtJitJ3EmtwtXWE62J7QAByGcE3wAAAA==";
+    clientId = clientId;
+    listId = listId;
   };
 
   networking.firewall.allowedTCPPorts = [ port ];
