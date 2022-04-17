c2ddf39871685201a5f1801f8d27823b7f6f288e
Asmund Berge <asmund@berge.id>
2022-04-17 15:15:00 +0200

    clean up mook.nix

--- machine/mook.nix
+++ machine/mook.nix
@@ -1,12 +1,8 @@
 { config, pkgs, ... }:
 
-let
-	  alsSecrets = import /run/keys/als-secrets.nix;
-
-	in
 {
   imports =
-    [ ../module/als/service.nix
+    [ ../module/als
       ../module/mediawiki.nix
       ../module/avahi.nix
       ../module/neovim
@@ -14,20 +10,9 @@ let
 
   environment.systemPackages = with pkgs; [
     git
-    neovim 
   ];
 
   services.openssh.enable = true;
-  
-
-  services.als = {
-    enable = true;
-    port = 8002;
-    clientId = "029cc178-e604-4d7f-a17c-a41f92c3182c";
-    accessToken = alsSecrets.accessToken;
-    refreshToken = alsSecrets.refreshToken;
-    listId = "AQMkADAwATNiZmYAZC0xZjgwLTE2YzUtMDACLTAwCgAuAAADQGkXSjdFSkyRkPd2Ueax6wEA-3jtJitJ3EmtwtXWE62J7QAByGcE3wAAAA==";
-  };
 
   networking.firewall.allowedTCPPorts = [ 22 80 443 ];
 }
