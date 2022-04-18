8b6825ab5dd85f8f5a94d392462b87d11d59a71e
Asmund Berge <asmund@berge.id>
2022-04-18 17:36:07 +0200

    PMS: Use signal-cli from unstable

--- module/pms/default.nix
+++ module/pms/default.nix
@@ -3,12 +3,13 @@
   with lib;
 
 let
+  unstable = fetchTarball "https://github.com/NixOS/nixpkgs/archive/759a1f7742c76594955b8fc1c04b66dc409b8ff2.tar.gz";
   cfg = config.services.pms;
   src = pkgs.fetchFromGitHub {
     owner = "powermosfet";
     repo  = "pms";
-    rev = "08a000891fad5f1ed04f1bc3085798365798bf56";
-    sha256 = "0vsz091725fbhj742hm6dbybb5bnbim2bx7bvj3di9j5629cx45v";
+    rev = "3695e9b6f4ca341b99407fb68223c206999faee2";
+    sha256 = "0000000000000000000000000000000000000000000000000000";
   };
   pms = import src { };
 in
@@ -47,7 +48,7 @@ in
       after = [ "network.target" ];
       environment = {
         APP_PORT = toString cfg.port;
-        SIGNAL_CLI = "${pkgs.signal-cli}/bin/signal-cli";
+        SIGNAL_CLI = "${unstable.signal-cli}/bin/signal-cli";
         SIGNAL_SENDER = cfg.sender;
 	SIGNAL_RECIPIENT = cfg.recipient;
 	SIGNAL_LOG_RECIPIENT = cfg.logRecipient;
