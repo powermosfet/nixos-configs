9eb5c0bef4dfe22f37ebb0f00981f325b23dd212
Asmund Berge <asmund@berge.id>
2022-04-18 11:54:09 +0200

    Move into config attribute

--- module/mediawiki.nix
+++ module/mediawiki.nix
@@ -13,80 +13,84 @@ in
     [ 
     ];
 
-  options.services.mediawiki = {
-    internalPort = mkOption {
-      description = "internal network port used by reverse proxy";
-      type = types.int;
+  options = {
+    services.mediawiki = {
+      internalPort = mkOption {
+        description = "internal network port used by reverse proxy";
+        type = types.int;
+      };
     };
   };
 
-  services.httpd.adminAddr = email;
+  config = {
+    services.httpd.adminAddr = email;
 
-  services.mediawiki = {
-    enable = true;
-    name = "Berge wiki";
-    database = {
-      type = "mysql";
-      createLocally = true;
-    };
-    virtualHost = {
-      hostName = hostName;
-      adminAddr = email;
-      listen =  [
-        {
-          ip = "127.0.0.1";
-          port = internalPort;
-          ssl = false;
-        }
-      ];
-    };
-    extensions = {
-      ParserFunctions = null;
-      CategoryTree = null;
-      VisualEditor = null;
-      WikiEditor = null;
-    };
-    extraConfig = ''
-      $wgLanguageCode = 'no';
-      $wgNamespacesWithSubpages[NS_MAIN] = 1;
+    services.mediawiki = {
+      enable = true;
+      name = "Berge wiki";
+      database = {
+        type = "mysql";
+        createLocally = true;
+      };
+      virtualHost = {
+        hostName = hostName;
+        adminAddr = email;
+        listen =  [
+          {
+            ip = "127.0.0.1";
+            port = internalPort;
+            ssl = false;
+          }
+        ];
+      };
+      extensions = {
+        ParserFunctions = null;
+        CategoryTree = null;
+        VisualEditor = null;
+        WikiEditor = null;
+      };
+      extraConfig = ''
+        $wgLanguageCode = 'no';
+        $wgNamespacesWithSubpages[NS_MAIN] = 1;
 
       # Disable reading by anonymous users
-      $wgGroupPermissions['*']['read'] = false;
-      
+        $wgGroupPermissions['*']['read'] = false;
+
       # Disable anonymous editing
-      $wgGroupPermissions['*']['edit'] = false;
-      
+        $wgGroupPermissions['*']['edit'] = false;
+
       # Prevent new user registrations except by sysops
-      $wgGroupPermissions['*']['createaccount'] = false;
+        $wgGroupPermissions['*']['createaccount'] = false;
 
-      $wgLogo = "$wgUploadPath/images/d/d4/Logo.jpg";
+        $wgLogo = "$wgUploadPath/images/d/d4/Logo.jpg";
       '';
-  };
-
-  services.nginx.virtualHosts."${hostName}" = {
-    enableACME = true;
-    forceSSL = true;
-    locations."/" = {
-      proxyPass = "http://127.0.0.1:${builtins.toString(internalPort)}";
-      proxyWebsockets = true;
     };
-  };
 
-  security.acme.email = email;
+    services.nginx.virtualHosts."${hostName}" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:${builtins.toString(internalPort)}";
+        proxyWebsockets = true;
+      };
+    };
 
-  services.parsoid = {
-    enable = true;
-    wikis = [ "http://localhost/api.php" ];
-  };
+    security.acme.email = email;
 
-  services.mysql = {
-    enable = true;
-  };
+    services.parsoid = {
+      enable = true;
+      wikis = [ "http://localhost/api.php" ];
+    };
 
-  services.mysqlBackup = {
-    enable = true;
-    databases = [ "mediawiki" ];
+    services.mysql = {
+      enable = true;
+    };
+
+    services.mysqlBackup = {
+      enable = true;
+      databases = [ "mediawiki" ];
+    };
+
+    networking.firewall.allowedTCPPorts = [ 80 443 ];
   };
-  
-  networking.firewall.allowedTCPPorts = [ 80 443 ];
 }
