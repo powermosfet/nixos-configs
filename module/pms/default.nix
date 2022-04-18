973ea9e199dae895f2d655310aa526c33edc1aa0
Asmund Berge <asmund@berge.id>
2022-04-18 16:11:40 +0200

    PMS: Update sha

--- module/pms/default.nix
+++ module/pms/default.nix
@@ -8,33 +8,35 @@ let
     owner = "powermosfet";
     repo  = "pms";
     rev = "0425ae6e6aa1b6a82267117d8a0a76e0d0718636";
-    sha256 = "0000000000000000000000000000000000000000000000000000";
+    sha256 = "0fr91vf3g2v351ihzf01a5y73qyv19lwpgp1i3vg1r2qa5k1f8v1";
   };
   pms = import src { };
+in
+{
+  options = {
+    services.pms = {
+      enable = mkEnableOption "pms service";
 
-in {
-  options.services.pms = {
-    enable = mkEnableOption "pms service";
+      port = mkOption {
+        description = "Network port to listen on";
+        default = 8080;
+        type = types.int;
+      };
 
-    port = mkOption {
-      description = "Network port to listen on";
-      default = 8080;
-      type = types.int;
-    };
+      sender = mkOption {
+        description = "Signal sender id (phone number)";
+        type = types.str;
+      };
 
-    sender = mkOption {
-      description = "Signal sender id (phone number)";
-      type = types.str;
-    };
+      recipient = mkOption {
+        description = "main recipient";
+        type = types.str;
+      };
 
-    recipient = mkOption {
-      description = "main recipient";
-      type = types.str;
-    };
-
-    logRecipient = mkOption {
-      description = "log recipient";
-      type = types.str;
+      logRecipient = mkOption {
+        description = "log recipient";
+        type = types.str;
+      };
     };
   };
 
