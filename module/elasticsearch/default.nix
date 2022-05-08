f7711b29145545fa944c8a05a5cd62404700c97b
Asmund Berge <asmund@berge.id>
2022-05-08 15:53:38 +0200

    Elasticsearch: Set java version using overlay

--- module/elasticsearch/default.nix
+++ module/elasticsearch/default.nix
@@ -1,20 +1,24 @@
 { pkgs, ... }:
 
+let
+  myElasticsearch = pkgs.elasticsearch.override {
+    jre_headless = pkgs.oraclejre8;
+  };
+in
 {
   imports =
     [
     ];
 
   config = {
-    programs.java = {
-      package = pkgs.oraclejre;
-    };
+    nixpkgs.config.allowUnfree = true;
 
     services.elasticsearch = {
       enable = true;
 
-      package = pkgs.elasticsearch;
+      package = myElasticsearch;
       plugins = [
+        pkgs.elasticsearchPlugins.ingest-attachment
         ];
     };
   };
