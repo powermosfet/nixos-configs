11d3f2e6e5c5e3f3c881c88c446b861b8a255643
Asmund Berge <asmund@berge.id>
2022-06-28 17:03:02 +0200

    Minecraft: Upgrade to 1.19

--- module/minecraft/default.nix
+++ module/minecraft/default.nix
@@ -1,11 +1,15 @@
 { pkgs, ... }:
 
+let
+  unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/3b6bc4b69ca7eec695291af31d8878071e0e084d.tar.gz") { };
+in
 {
   config = {
     nixpkgs.config.allowUnfree = true;
 
     services.minecraft-server = {
       enable = true;
+      package = unstable.minecraft-server;
       openFirewall = true;
       eula = true;
     };
