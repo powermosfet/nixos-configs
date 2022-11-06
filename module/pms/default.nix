f9a94844633d695e581b3e131316a20c02a8cbf3
Asmund Berge <asmund@berge.id>
2022-11-06 15:27:44 +0100

    PMS: Add signal receive timer

--- module/pms/default.nix
+++ module/pms/default.nix
@@ -42,20 +42,40 @@ in
   };
 
   config = mkIf cfg.enable {
-    systemd.services.pms = {
-      description = "PMS Service (Personal Memo Sender).";
-      wantedBy = [ "multi-user.target" ];
-      after = [ "network.target" ];
-      environment = {
-        APP_PORT = toString cfg.port;
-        SIGNAL_CLI = "${unstable.signal-cli}/bin/signal-cli";
-        SIGNAL_SENDER = cfg.sender;
-	SIGNAL_RECIPIENT = cfg.recipient;
-	SIGNAL_LOG_RECIPIENT = cfg.logRecipient;
-	SIGNAL_CONFIG_PATH = "/root/.local/share/signal-cli";
+    systemd.services = {
+      pms = {
+        description = "PMS Service (Personal Memo Sender).";
+        wantedBy = [ "multi-user.target" ];
+        after = [ "network.target" ];
+        environment = {
+          APP_PORT = toString cfg.port;
+          SIGNAL_CLI = "${unstable.signal-cli}/bin/signal-cli";
+          SIGNAL_SENDER = cfg.sender;
+          SIGNAL_RECIPIENT = cfg.recipient;
+          SIGNAL_LOG_RECIPIENT = cfg.logRecipient;
+          SIGNAL_CONFIG_PATH = "/root/.local/share/signal-cli";
+        };
+        serviceConfig = {
+          ExecStart = "${pms}/bin/pms";
+        };
       };
-      serviceConfig = {
-        ExecStart = "${pms}/bin/pms";
+      pms-receive-timer = {
+        serviceConfig.Type = "oneshot";
+        path = with pkgs; [ bash ];
+        script = ''
+          ${unstable.signal-cli}/bin/signal-cli -a ${cfg.sender} receive
+        '';
+      };
+    };
+
+    systemd.timers = {
+      pms-receive-timer = {
+        wantedBy = [ "timers.target" ];
+        partOf = [ "pms-receive-timer.service" ];
+        timerConfig = {
+          OnCalendar = "*:0/1";
+          Unit = "pms-receive-timer.service";
+        };
       };
     };
 
