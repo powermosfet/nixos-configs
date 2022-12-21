3e92b7183db4a64dc462559a9cb15164ef7218fd
Asmund Berge <powermosfet@users.noreply.github.com>
2022-12-21 18:48:40 +0100

    Update default.nix

--- module/minecraft/default.nix
+++ module/minecraft/default.nix
@@ -1,15 +1,12 @@
 { pkgs, config, ... }:
 
-let
-  unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/f2537a505d45c31fe5d9c27ea9829b6f4c4e6ac5.tar.gz") { config = config.nixpkgs.config; };
-in
 {
   config = {
     nixpkgs.config.allowUnfree = true;
 
     services.minecraft-server = {
       enable = true;
-      package = unstable.minecraft-server;
+      package = pkgs.minecraft-server;
       openFirewall = true;
       eula = true;
     };
