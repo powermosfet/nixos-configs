811bb20ccbb744d00de99dc2cb36e19eb3e15bd8
Asmund Berge <asmund@conta.no>
2023-01-23 08:54:32 +0100

    Neovim: Make vimwiki mappings global

--- module/neovim/config/lua/sharedconfig/plugins/telescope/vimwiki.lua
+++ module/neovim/config/lua/sharedconfig/plugins/telescope/vimwiki.lua
@@ -1,12 +1,5 @@
 require('telescope').load_extension('vimwiki')
 
-vim.api.nvim_create_augroup("telescopeVimwikiAutoCommands", { clear = true })
-vim.api.nvim_create_autocmd("FileType", {
-    group = "telescopeVimwikiAutoCommands",
-    pattern = { "vimwiki" },
-    callback = function()
-        vim.keymap.set('n', '<leader>ff', ':lua require("telescope").extensions.vimwiki.vimwiki()<cr>', { buffer = true })
-        vim.keymap.set('n', '<leader>sl', ':lua require("telescope").extensions.vw.live_grep()<cr>',    { buffer = true })
-        vim.keymap.set('n', '<leader><leader>', ':VimwikiToggleListItem<cr>',    { buffer = true })
-    end,
-})
+vim.keymap.set('n', '<leader>wf', ':Telescope vimwiki<cr>')
+vim.keymap.set('n', '<leader>w/', ':Telescope vimwiki live_grep<cr>')
+vim.keymap.set('n', '<leader>wl', ':Telescope vimwiki links<cr>')
