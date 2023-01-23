ac60c21d22953f1bcd40ff06d4f7fe86693d75c0
Asmund Berge <asmund@conta.no>
2023-01-23 09:05:31 +0100

    Neovim: Use PageDown/PageUp for buffer switching

--- module/neovim/config/lua/sharedconfig/keymaps.lua
+++ module/neovim/config/lua/sharedconfig/keymaps.lua
@@ -1,76 +1,76 @@
 return {
-  ["<BS>"]    = [[:BufferPrevious<cr>]],
-  ["<TAB>"]   = [[:BufferNext<cr>]],
-  ["[g"]          = [[:lua vim.diagnostic.goto_prev()<CR>]],
-  ["]g"]          = [[:lua vim.diagnostic.goto_next()<CR>]],
+  ["<PageUp>"]       = [[:BufferPrevious<cr>]],
+  ["<PageDown>"]     = [[:BufferNext<cr>]],
+  ["[g"]             = [[:lua vim.diagnostic.goto_prev()<CR>]],
+  ["]g"]             = [[:lua vim.diagnostic.goto_next()<CR>]],
   -- Diagnostics
-  ["d"]         = {
-    ["n"]       = [[:lua vim.diagnostic.goto_next()<cr>]],
-    ["p"]       = [[:lua vim.diagnostic.goto_prev()<cr>]],
-    ["l"]       = [[:Telescope diagnostics bufnr=0<cr>]],
-    ["L"]       = [[:Telescope diagnostics<cr>]],
+  ["d"]              = {
+    ["n"]            = [[:lua vim.diagnostic.goto_next()<cr>]],
+    ["p"]            = [[:lua vim.diagnostic.goto_prev()<cr>]],
+    ["l"]            = [[:Telescope diagnostics bufnr=0<cr>]],
+    ["L"]            = [[:Telescope diagnostics<cr>]],
   },
   -- Bookmarks
-  ["m"]         = {
-    ["t"]       = [[:Telescope vim_bookmarks current_file<cr>]],
-    ["T"]       = [[:Telescope vim_bookmarks all<cr>]],
+  ["m"]              = {
+    ["t"]            = [[:Telescope vim_bookmarks current_file<cr>]],
+    ["T"]            = [[:Telescope vim_bookmarks all<cr>]],
   },
-  ["<leader>"]    = {
-    ["v"]         = [[:tabe <c-r>=resolve($MYVIMRC)<cr><cr>]],
-    ["n"]         = [[:noh<cr>]],
-    ["c"]         = [[:close<cr>]],
-    [","]         = [[:tabp<cr>]],
-    ["."]         = [[:tabn<cr>]],
+  ["<leader>"]       = {
+    ["v"]            = [[:tabe <c-r>=resolve($MYVIMRC)<cr><cr>]],
+    ["n"]            = [[:noh<cr>]],
+    ["c"]            = [[:close<cr>]],
+    [","]            = [[:tabp<cr>]],
+    ["."]            = [[:tabn<cr>]],
     -- Search
-    ["s"]         = {
-      ["w"]       = [[:Telescope grep_string<cr>]],
-      ["l"]       = [[:Telescope live_grep<cr>]],
+    ["s"]            = {
+      ["w"]          = [[:Telescope grep_string<cr>]],
+      ["l"]          = [[:Telescope live_grep<cr>]],
     },
     -- Buffer
-    ["b"]         = {
-      ["l"]       = [[:Telescope buffers<cr>]],
-      ["o"]       = [[:b#<cr>]],
-      ["d"]       = [[:BufferDelete<cr>]],
-      ["c"]       = [[:BufferClose<cr>]],
-      ["O"]       = [[:BufferCloseAllButCurrentOrPinned<cr>]],
-      ["p"]       = [[:BufferPin<cr>]],
-      ["j"]       = [[:BufferPick<cr>]],
-      ["<BS>"]    = [[:BufferMovePrevious<cr>]],
-      ["<TAB>"]   = [[:BufferMoveNext<cr>]],
+    ["b"]            = {
+      ["l"]          = [[:Telescope buffers<cr>]],
+      ["o"]          = [[:b#<cr>]],
+      ["d"]          = [[:BufferDelete<cr>]],
+      ["c"]          = [[:BufferClose<cr>]],
+      ["O"]          = [[:BufferCloseAllButCurrentOrPinned<cr>]],
+      ["p"]          = [[:BufferPin<cr>]],
+      ["j"]          = [[:BufferPick<cr>]],
+      ["<PageUp>"]   = [[:BufferMovePrevious<cr>]],
+      ["<PageDown>"] = [[:BufferMoveNext<cr>]],
     },
     -- Files
-    ["f"]         = {
-      ["f"]       = [[:Telescope find_files<cr>]],
-      ["b"]       = [[:Telescope file_browser<cr>]],
-      ["t"]       = [[:NvimTreeFocus<cr>]],
-      ["T"]       = [[:NvimTreeFindFile<cr>]],
+    ["f"]            = {
+      ["f"]          = [[:Telescope find_files<cr>]],
+      ["b"]          = [[:Telescope file_browser<cr>]],
+      ["t"]          = [[:NvimTreeFocus<cr>]],
+      ["T"]          = [[:NvimTreeFindFile<cr>]],
     },
     -- Git
-    ["g"]         = {
-      ["<space>"] = [[:G ]],
-      ["b"]       = [[:Telescope git_branches<cr>]],
-      ["c"]       = [[:Telescope git_bcommits<cr>]],
-      ["s"]       = [[:G<cr>]],
-      ["f"]       = [[:Git fetch --prune<cr>]],
-      ["p"]       = [[:Git pull --ff-only<cr>]],
-      ["o"]       = [[:Git checkout ]],
-      ["n"]       = [[/\(<<<<<<<\|=======\||||||||\|>>>>>>>\)<cr>]],
-      ["h"]       = [[:GitGutterPreviewHunk<cr>]]
+    ["g"]            = {
+      ["<space>"]    = [[:G ]],
+      ["b"]          = [[:Telescope git_branches<cr>]],
+      ["c"]          = [[:Telescope git_bcommits<cr>]],
+      ["s"]          = [[:G<cr>]],
+      ["f"]          = [[:Git fetch --prune<cr>]],
+      ["p"]          = [[:Git pull --ff-only<cr>]],
+      ["o"]          = [[:Git checkout ]],
+      ["n"]          = [[/\(<<<<<<<\|=======\||||||||\|>>>>>>>\)<cr>]],
+      ["h"]          = [[:GitGutterPreviewHunk<cr>]]
     },
-    ["l"]         = {
-      ["z"]       = [[:LspRestart<CR>]],
-      ["f"]       = [[:lua vim.lsp.buf.formatting()<CR>]],
-      ["D"]       = [[:lua vim.lsp.buf.declaration()<CR>]],
-      ["d"]       = [[:lua vim.lsp.buf.definition()<CR>]],
-      ["h"]       = [[:lua vim.lsp.buf.hover()<CR>]],
-      ["i"]       = [[:lua vim.lsp.buf.implementation()<CR>]],
-      ["R"]       = [[:lua vim.lsp.buf.rename()<CR>]],
-      ["a"]       = [[:lua vim.lsp.buf.code_action()<CR>]],
-      ["r"]       = [[:Telescope lsp_references<CR>]],
-      ["gb"]      = [[:Telescope lsp_document_diagnostics<CR>]],
-      ["gw"]      = [[:Telescope lsp_workspace_diagnostics<CR>]],
-      ["/"]       = [[:Telescope lsp_document_symbols<CR>]],
-      ["g/"]      = [[:Telescope lsp_workspace_symbols<CR>]]
+    ["l"]            = {
+      ["z"]          = [[:LspRestart<CR>]],
+      ["f"]          = [[:lua vim.lsp.buf.formatting()<CR>]],
+      ["D"]          = [[:lua vim.lsp.buf.declaration()<CR>]],
+      ["d"]          = [[:lua vim.lsp.buf.definition()<CR>]],
+      ["h"]          = [[:lua vim.lsp.buf.hover()<CR>]],
+      ["i"]          = [[:lua vim.lsp.buf.implementation()<CR>]],
+      ["R"]          = [[:lua vim.lsp.buf.rename()<CR>]],
+      ["a"]          = [[:lua vim.lsp.buf.code_action()<CR>]],
+      ["r"]          = [[:Telescope lsp_references<CR>]],
+      ["gb"]         = [[:Telescope lsp_document_diagnostics<CR>]],
+      ["gw"]         = [[:Telescope lsp_workspace_diagnostics<CR>]],
+      ["/"]          = [[:Telescope lsp_document_symbols<CR>]],
+      ["g/"]         = [[:Telescope lsp_workspace_symbols<CR>]]
     }
   }
 }
