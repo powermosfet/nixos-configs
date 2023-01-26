a3715ab4f8c13bd19ecccccd5e3469315b64b0d5
Asmund Berge <asmund@conta.no>
2023-01-26 16:28:25 +0100

    Mediawiki: Try using //

--- module/mediawiki/default.nix
+++ module/mediawiki/default.nix
@@ -9,7 +9,7 @@ let
   internalPort = config.services.mediawiki.internalPort;
   smtpPassword = config.services.mediawiki.smtpPassword;
 
-  myMediaWiki = pkgs.mediawiki.override {
+  myMediaWiki = pkgs.mediawiki // {
     nativeBuildInputs = with pkgs; [
       # Packages needed by the Diagrams extension
       graphviz
