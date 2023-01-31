f1a484807de687c28489b3d589c2b98a28dd2e34
Asmund Berge <asmund@berge.id>
2023-01-31 11:52:13 +0100

    Dokuwiki: Fix sha

--- module/dokuwiki/default.nix
+++ module/dokuwiki/default.nix
@@ -36,7 +36,7 @@ let
     name = "pagelist";
     src = pkgs.fetchzip {
       url = "https://github.com/dokufreaks/plugin-pagelist/archive/refs/heads/master.zip";
-      sha256 = "sha256-0ru90nDvePqdcdwVFmeXKatbmXVORx3gtpTRsrdRRRA=";
+      sha256 = "sha256-ATk/qjsFrAOZpfu79Pp+YWtCYnkEJ7fSaTHanOS5wMg=";
     };
     sourceRoot = ".";
     installPhase = "mkdir -p $out; cp -R source/* $out/";
