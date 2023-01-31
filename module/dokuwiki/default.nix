0fa489fe4851bb1d4039c9510f6099d14fc7486a
Asmund Berge <asmund@berge.id>
2023-01-31 14:00:19 +0100

    Dokuwiki: Fix sha

--- module/dokuwiki/default.nix
+++ module/dokuwiki/default.nix
@@ -36,7 +36,7 @@ let
     name = "cloud";
     src = pkgs.fetchzip {
       url = "https://github.com/dokufreaks/plugin-cloud/archive/refs/heads/master.zip";
-      sha256 = "sha256-SahmPJOkS41RTkPBgmFhaycsUjCbiSS7+/N4prUAGEE=";
+      sha256 = "sha256-CiZAE6B4XFCRI7P872AUdZHz6qcMr7x+nY+WK5WbSy8=";
     };
     sourceRoot = ".";
     installPhase = "mkdir -p $out; cp -R source/* $out/";
