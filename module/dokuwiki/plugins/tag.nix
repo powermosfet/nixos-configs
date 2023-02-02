27adc0f99da07910f3e4c132bc2058d82ac37b5b
Asmund Berge <asmund@berge.id>
2023-02-02 17:42:12 +0100

    Dokuwiki: Tag plugin: Fixing revision

--- module/dokuwiki/plugins/tag.nix
+++ module/dokuwiki/plugins/tag.nix
@@ -5,7 +5,7 @@ pkgs.stdenv.mkDerivation {
   src = pkgs.fetchFromGitHub {
     owner = "dokufreaks";
     repo  = "plugin-tag";
-    rev = "d5e67bf3e1824ea785699dec107816ec";
+    rev = "d4b003e4ebaf0a58926ec3efbd78798e5876153d";
     sha256 = "10ry3am7fqfylz0mda9lybp681x22js1gf2xh5w85jhqlmvzizid";
   };
   sourceRoot = ".";
