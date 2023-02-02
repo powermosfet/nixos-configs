0a1ddc39c84aa235a36c00ff635b8bef343d0813
Asmund Berge <asmund@berge.id>
2023-02-02 19:12:28 +0100

    Dokuwiki: Fix sha

--- module/dokuwiki/plugins/imgpaste.nix
+++ module/dokuwiki/plugins/imgpaste.nix
@@ -6,5 +6,5 @@ import ./helper {
   owner = "cosmocode";
   repo  = "dokuwiki-plugin-imgpaste";
   rev = "20d44e161fcac47e4bdeb3d427ea83a64b0afe76";
-  sha256 = "ayUnYhpx8jOQULs2lsR7+TeXRUHK8110vhfw0BcQX22=";
+  sha256 = "WD0PgEE6jTZuc6iUqOxEqavhgP/8J6ikc9O+fMA1E3E=";
 }
