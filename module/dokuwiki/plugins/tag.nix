214efc82174394bcd47675dece3eca03972b353c
Asmund Berge <asmund@berge.id>
2023-02-02 17:44:48 +0100

    Dokuwiki: Tag plugin: Fix sha

--- module/dokuwiki/plugins/tag.nix
+++ module/dokuwiki/plugins/tag.nix
@@ -6,7 +6,7 @@ pkgs.stdenv.mkDerivation {
     owner = "dokufreaks";
     repo  = "plugin-tag";
     rev = "d4b003e4ebaf0a58926ec3efbd78798e5876153d";
-    sha256 = "10ry3am7fqfylz0mda9lybp681x22js1gf2xh5w85jhqlmvzizid";
+    sha256 = "0ru90nDvePqdcdwVFmeXKatbmXVORx3gtpTRsrdRRMA=";
   };
   sourceRoot = ".";
   installPhase = "mkdir -p $out; cp -R source/* $out/";
