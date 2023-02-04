67db7ea9fc986135c8fb9a2859121df8e2200fd1
Asmund Berge <asmund@berge.id>
2023-02-04 21:24:31 +0100

    Borg: Fix postgresqlbackup

--- module/postgresql/backup/default.nix
+++ module/postgresql/backup/default.nix
@@ -4,18 +4,19 @@
 
 {
   config = {
-    services.postgresqlBackup = {
-      enable = true;
-      backupAll = false;
-    };
+    services = {
+      postgresqlBackup = {
+        enable = true;
+        backupAll = false;
+      };
 
-
-    services.borgbackup.jobs."postgresql-gilli" = {
-      paths = services.postgresqlBackup.location;
-      encryption.mode = "none";
-      environment.BORG_RSH = "ssh -i /root/.ssh/id_borg-main";
-      repo = "borg@gilli.local:.";
-      compression = "auto,zstd";
+      borgbackup.jobs."postgresql-gilli" = {
+        paths = config.services.postgresqlBackup.location;
+        encryption.mode = "none";
+        environment.BORG_RSH = "ssh -i /root/.ssh/id_borg-main";
+        repo = "borg@gilli.local:.";
+        compression = "auto,zstd";
+      };
     };
   };
 }
