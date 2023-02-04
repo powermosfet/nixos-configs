0d1cadbd77a1ff9f5a48f58fea9ba8964256020d
Asmund Berge <asmund@berge.id>
2023-02-04 21:41:13 +0100

    Borg: Job names cannot have dots in them

--- module/dokuwiki/backup/default.nix
+++ module/dokuwiki/backup/default.nix
@@ -3,7 +3,7 @@
 {
   services.borgbackup.jobs = builtins.mapAttrs (site: _: 
   {
-    paths = config.services.dokuwiki."${site}".stateDir;
+    paths = config.services.dokuwiki."${builtins.replaceStrings ["."] ["-"] site}".stateDir;
     encryption.mode = "none";
     environment.BORG_RSH = "ssh -i /root/.ssh/id_borg-main";
     repo = "borg@gilli.local:.";
