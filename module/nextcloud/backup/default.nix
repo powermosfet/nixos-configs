637980a77cadd9796b9f9b0c09d7e8dc88f17454
Asmund Berge <asmund@berge.id>
2023-02-04 09:55:46 +0100

    Borg: Fix syntax error in mook config

--- module/nextcloud/backup/default.nix
+++ module/nextcloud/backup/default.nix
@@ -2,7 +2,7 @@
 
 {
   services.borgbackup.jobs."nextcloud-gilli" = {
-    paths = options.services.nextcloud.home
+    paths = options.services.nextcloud.home;
     encryption.mode = "none";
     environment.BORG_RSH = "ssh -i /root/.ssh/id_borg-nextcloud";
     repo = "ssh://backup@gilli.local:/mnt/passport/borgbackup/nextcloud";
