646beb8e6d9697c4c8e8624f83249eacb72cc9fa
Asmund Berge <asmund@berge.id>
2023-02-04 21:43:56 +0100

    Borg: Construct attributes for each site

--- module/dokuwiki/backup/default.nix
+++ module/dokuwiki/backup/default.nix
@@ -3,11 +3,13 @@
 {
   services.borgbackup.jobs = builtins.mapAttrs (site: _: 
   {
-    paths = config.services.dokuwiki."${builtins.replaceStrings ["."] ["-"] site}".stateDir;
-    encryption.mode = "none";
-    environment.BORG_RSH = "ssh -i /root/.ssh/id_borg-main";
-    repo = "borg@gilli.local:.";
-    compression = "auto,zstd";
+    "${builtins.replaceStrings ["."] ["-"] site}" = {
+      paths = config.services.dokuwiki."${site}".stateDir;
+      encryption.mode = "none";
+      environment.BORG_RSH = "ssh -i /root/.ssh/id_borg-main";
+      repo = "borg@gilli.local:.";
+      compression = "auto,zstd";
+    };
   }) config.services.dokuwiki.sites;
 }
 
