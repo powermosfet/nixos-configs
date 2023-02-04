ae19d494ecd37a3a4c723a4bf75b3872653cc4ac
Asmund Berge <asmund@berge.id>
2023-02-04 10:06:41 +0100

    Borg: Fix repo path

--- module/nextcloud/backup/default.nix
+++ module/nextcloud/backup/default.nix
@@ -5,7 +5,7 @@
     paths = config.services.nextcloud.home;
     encryption.mode = "none";
     environment.BORG_RSH = "ssh -i /root/.ssh/id_borg-nextcloud";
-    repo = "ssh://backup@gilli.local:/mnt/passport/borgbackup/nextcloud";
+    repo = "ssh://borg@gilli.local//mnt/passport/borgbackup/nextcloud";
     compression = "auto,zstd";
   };
 }
