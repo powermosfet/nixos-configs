f97cd7851402cac46b033e598d84cdb5428f1120
Asmund Berge <asmund@berge.id>
2023-02-05 10:33:50 +0100

    Borg: Fix curly braces

--- module/borg/jobs/main/default.nix
+++ module/borg/jobs/main/default.nix
@@ -3,19 +3,17 @@
 {
   options = {
     backup.paths = mkOption {
-        description = "directories to back up";
-        type = types.listOf types.path;
-      };
+      description = "directories to back up";
+      type = types.listOf types.path;
     };
   };
 
   config = {
     services.borgbackup.jobs."main-gilli" = {
-        encryption.mode = "none";
-        environment.BORG_RSH = "ssh -i /root/.ssh/id_borg-main-gilli";
-        repo = "borg@gilli.local:.";
-        compression = "auto,zstd";
-      };
+      encryption.mode = "none";
+      environment.BORG_RSH = "ssh -i /root/.ssh/id_borg-main-gilli";
+      repo = "borg@gilli.local:.";
+      compression = "auto,zstd";
     };
   };
 }
