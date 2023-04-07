6c64843582b7a4a1c495279a9941def60f329ba6
Asmund Berge <asmund@berge.id>
2023-04-07 18:33:32 +0200

    Borg backup: Update job uri with correct port

--- module/borg/jobs/main/default.nix
+++ module/borg/jobs/main/default.nix
@@ -36,7 +36,7 @@
       paths = config.backup.paths;
       encryption.mode = "keyfile";
       environment.BORG_RSH = "ssh -i /root/.ssh/id_borg-main-agent25";
-      repo = "borg@agent25.berge.id:.";
+      repo = "ssh://borg@agent25.berge.id:222";
       compression = "auto,zstd";
       startAt = "04:00";
     };
