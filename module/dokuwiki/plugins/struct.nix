c7cec1e6b3983560e17a10e92c01a7ddf4f086c5
Asmund Berge <powermosfet@users.noreply.github.com>
2023-06-23 08:55:20 +0200

    Dokuwiki: Fix struct.nix SHA

--- module/dokuwiki/plugins/struct.nix
+++ module/dokuwiki/plugins/struct.nix
@@ -6,5 +6,5 @@ import ./helper {
   owner = "cosmocode";
   repo  = "dokuwiki-plugin-struct";
   rev = "6e2eda8d1f1070753fd032eca9541245be3681b9";
-  sha256 = "sha256-ALgpJIwHt8FfW1YWWTs7XRwDVrGEy+YwGA5kHQYNnGE=";
+  sha256 = "sha256-1SGyPhMVkho+Mfks5RxxzCbYDlj+SGSpN7RPBCWX7Vw=";
 }
