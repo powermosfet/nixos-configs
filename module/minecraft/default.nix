f1c25784cbd0a463aa3ad241b811d73fa90e218c
Asmund Berge <powermosfet@users.noreply.github.com>
2023-10-26 15:47:38 +0200

    Minecraft: Upgrade to 1.20.1

--- module/minecraft/default.nix
+++ module/minecraft/default.nix
@@ -1,7 +1,7 @@
 { pkgs, config, ... }:
   
 let
-  hash = "293a28df6d7ff3dec1e61e37cc4ee6e6c0fb0847";
+  hash = "8efd5d1e283604f75a808a20e6cde0ef313d07d4";
   unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/${hash}.tar.gz") { config = config.nixpkgs.config; };
 in
 {
