54be47d15a0e42aa09e77ed9e3bf3f248eb6130d
Asmund Berge <asmund@conta.no>
2024-01-18 16:07:39 +0100

    Neovim: Fix vimwiki config

--- module/neovim/config/lua/sharedconfig/plugins/vimwiki.lua
+++ module/neovim/config/lua/sharedconfig/plugins/vimwiki.lua
@@ -1,5 +1,15 @@
 vim.g.vimwiki_dir_link="index"
 
+vim.g.vimwiki_list = {
+    {
+        path = "~/vimwiki",
+        syntax = "markdown",
+        ext = ".md",
+        links_space_char = "_",
+        auto_tags = 1,
+    },
+}
+
 vim.api.nvim_create_augroup("vimwikiAutoCommands", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
     group = "vimwikiAutoCommands",
@@ -9,34 +19,20 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.opt_local.shiftwidth = 2
         vim.keymap.set("i", "<Tab>", "<C-O>gll", { noremap = true, buffer = true })
         vim.keymap.set("i", "<S-Tab>", "<C-O>glh", { noremap = true, buffer = true })
-        vim.g.vimwiki_list = {
-            {
-                path = "~/vimwiki",
-                syntax = "markdown",
-                ext = ".md",
-                links_space_char = "_",
-                auto_tags = 1,
+
+        -- Syntax highlighting for code blocks
+        vim.g.vimwiki_syntax_plugins = {
+            codeblock = {
+                ["```lua"] = { parser = "lua" },
+                ["```python"] = { parser = "python" },
+                ["```javascript"] = { parser = "javascript" },
+                ["```bash"] = { parser = "bash" },
+                ["```shell"] = { parser = "bash" },
+                ["```mermaid"] = { parser = "mermaid" },
+                ["```html"] = { parser = "html" },
+                ["```css"] = { parser = "css" },
+                ["```c"] = { parser = "c" },
             },
-          }
-
-          -- Disable header levels keybindings so oil.nvim will work
-          vim.g.vimwiki_key_mappings = {
-              headers = 0,
-          }
-
-          -- Syntax highlighting for code blocks
-          vim.g.vimwiki_syntax_plugins = {
-              codeblock = {
-                  ["```lua"] = { parser = "lua" },
-                  ["```python"] = { parser = "python" },
-                  ["```javascript"] = { parser = "javascript" },
-                  ["```bash"] = { parser = "bash" },
-                  ["```shell"] = { parser = "bash" },
-                  ["```mermaid"] = { parser = "mermaid" },
-                  ["```html"] = { parser = "html" },
-                  ["```css"] = { parser = "css" },
-                  ["```c"] = { parser = "c" },
-              },
-          }
-      end
-  })
+        }
+    end
+})
