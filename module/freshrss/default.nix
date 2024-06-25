32c6a34df6a0cbd9750809e040f2ac4d3ac82a95
Asmund Berge <powermosfet@users.noreply.github.com>
2024-06-25 09:07:20 +0200

    FreshRSS: Use sqlite just to get going

--- module/freshrss/default.nix
+++ module/freshrss/default.nix
@@ -2,7 +2,7 @@
 
 {
   imports =
-    [ ../postgresql
+    [ 
     ];
 
   config = {
@@ -11,18 +11,6 @@
         enable = true;
         virtualHost = "freshrss.berge.id";
         baseUrl = "https://${config.services.freshrss.virtualHost}";
-        database = {
-          type = "pgsql";
-          host = "/run/postgresql";
-        };
-      };
-      postgresql = {
-        ensureDatabases = [ config.services.freshrss.database.name ];
-        ensureUsers = [
-          { name = config.services.freshrss.database.user;
-            ensureDBOwnership = true;
-          }
-        ];
       };
       nginx.virtualHosts."${config.services.freshrss.virtualHost}" = {
         enableACME = true;
