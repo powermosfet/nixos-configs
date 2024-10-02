d0fa03d34ea1f61cf226219d260222367897d78d
Asmund Berge <asmund@conta.no>
2024-10-02 07:03:56 +0200

    Neovim: Remove neorg

--- module/neovim/config/lua/sharedconfig/plugins.lua
+++ module/neovim/config/lua/sharedconfig/plugins.lua
@@ -142,26 +142,6 @@ return require('packer').startup(function(use)
     ft = {'lilypond'},
   }
   use 'mattn/calendar-vim'
-  use {
-    "nvim-neorg/neorg",
-    config = function()
-        require('neorg').setup {
-            load = {
-                ["core.defaults"] = {}, -- Loads default behaviour
-                ["core.concealer"] = {}, -- Adds pretty icons to your documents
-                ["core.dirman"] = { -- Manages Neorg workspaces
-                    config = {
-                        workspaces = {
-                            worknotes = "~/neorg-notes",
-                        },
-                    },
-                },
-            },
-        }
-    end,
-    run = ":Neorg sync-parsers",
-    requires = "nvim-lua/plenary.nvim",
-  }
 
 end)
 
