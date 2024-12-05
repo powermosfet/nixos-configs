46cba0e4a1b3ac731dd83022a69c3523df7e1f4e
Asmund Berge <powermosfet@users.noreply.github.com>
2024-12-05 08:44:18 +0100

    Elasticsearch: Remove oracle JRE 8

--- module/elasticsearch/default.nix
+++ module/elasticsearch/default.nix
@@ -1,22 +1,14 @@
 { pkgs, ... }:
 
-let
-  myElasticsearch = pkgs.elasticsearch.override {
-    jre_headless = pkgs.oraclejre8;
-  };
-in
 {
   imports =
     [
     ];
 
   config = {
-    nixpkgs.config.allowUnfree = true;
-
     services.elasticsearch = {
       enable = true;
 
-      package = myElasticsearch;
       plugins = [
         pkgs.elasticsearchPlugins.ingest-attachment
         ];
