1af45e47b43150c89adb6c654da2f0c26b84b808
Asmund Berge <asmund@berge.id>
2025-01-18 12:17:22 +0100

    Oauth2-proxy: Messing around

--- module/oauth2-proxy/default.nix
+++ module/oauth2-proxy/default.nix
@@ -14,7 +14,6 @@
 
       provider = "github";
       nginx.domain = "oauth.berge.id";
-      nginx.virtualHosts."oauth.berge.id".allowed_email_domains = [ "berge.id" ];
       email.domains = [ "berge.id" ];
     };
   };
