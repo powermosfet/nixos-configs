38d74c5626f8a6fa9033af993d82311c7122c191
Asmund Berge <asmund@berge.id>
2025-01-27 08:34:08 +0100

    Workout-tracker: Revert to std 1.8.1

--- module/workout-tracker/default.nix
+++ module/workout-tracker/default.nix
@@ -1,30 +1,5 @@
 { pkgs, config, ... }:
 
-let
-  pname = "workout-tracker";
-  version = "2.0.2";
-  src = pkgs.fetchFromGitHub {
-    owner = "jovandeginste";
-    repo = "workout-tracker";
-    rev = "refs/tags/v${version}";
-    hash = "sha256-m0f6iyexRRPXNGt56uaQrUmQiOuyb0ieze6CpY/QZfQ=";
-  };
-  assets = pkgs.buildNpmPackage {
-    pname = "${pname}-assets";
-    inherit version src;
-    npmDepsHash = "sha256-ntOQeTyTkYx1WILE+CV/BPCgT1T6zkBynCOeQWvhFr4=";
-    dontNpmBuild = true;
-    postPatch = ''
-      rm Makefile
-      '';
-    installPhase = ''
-      runHook preInstall
-      cp -r . "$out"
-      runHook postInstall
-      '';
-    makeCacheWritable = true;
-  };
-in
 {
   imports =
     [
@@ -33,9 +8,6 @@ in
   config = {
     services.workout-tracker = {
       enable = true;
-      package = pkgs.workout-tracker.overrideAttrs {
-        inherit version src assets;
-      };
       
       settings = {
         WT_REGISTRATION_DISABLED = "true";
