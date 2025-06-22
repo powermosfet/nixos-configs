faa105d895d16775cd47dade45c1c320932fdba2
Asmund Berge <asmund@berge.id>
2025-06-22 20:33:58 +0200

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "My shared NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }:
+    {
+      # Export your modules/configurations
+      machine = {
+        thinkpad = {
+          imports = [ ./machine/thinkpad ];
+        };
+        mook = {
+          imports = [ ./machine/mook ];
+        };
+        gilli = {
+          imports = [ ./machine/gilli ];
+        };
+      };
+    };
+}
