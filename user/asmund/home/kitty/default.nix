2adeadeb4672de20114e3f82199e519120753e5a
Asmund Berge <asmund@conta.no>
2025-06-26 09:14:49 +0200

    Kitty: Configure using home-manager options

--- user/asmund/home/kitty/default.nix
+++ user/asmund/home/kitty/default.nix
@@ -2,8 +2,18 @@
 
 {
   home.file = {
-    ".config/kitty/kitty.conf".source = ./kitty.conf;
     ".config/kitty/zellij.session".source = ./zellij.session;
     ".config/kitty/quickie.session".source = ./quickie.session;
+  };
+
+  programs.kitty = {
+    enable = true;
+
+    settings = {
+      font_size = 12.0;
+      disable_ligatures = "never";
+      confirm_os_window_close = 0;
+    };
+    font = pkgs.nerd-fonts.fira-code;
   };
 }
