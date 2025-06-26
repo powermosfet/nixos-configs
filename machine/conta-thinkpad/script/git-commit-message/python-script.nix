cffa486b69633bbfd26933624085e14876dbe38b
Asmund Berge <asmund@conta.no>
2025-06-26 07:16:29 +0200

    Conta-thinkpad: Remove semicolon

--- machine/conta-thinkpad/script/git-commit-message/python-script.nix
+++ machine/conta-thinkpad/script/git-commit-message/python-script.nix
@@ -1,20 +1,18 @@
 { pkgs }:
 
-(
-  pkgs.writeTextFile {
-    name = "git-commit-message.py";
-    text = ''
-#!/usr/bin/env python3
+(pkgs.writeTextFile {
+  name = "git-commit-message.py";
+  text = ''
+    #!/usr/bin/env python3
 
-import re, subprocess
+    import re, subprocess
 
-git_result = subprocess.run(["${pkgs.git}/bin/git", "rev-parse", "--abbrev-ref", "HEAD"], capture_output = True)
-branch_string = git_result.stdout.decode("UTF-8")
-result = re.compile("([^/]*)/([0-9]*)").match(branch_string)
+    git_result = subprocess.run(["${pkgs.git}/bin/git", "rev-parse", "--abbrev-ref", "HEAD"], capture_output = True)
+    branch_string = git_result.stdout.decode("UTF-8")
+    result = re.compile("([^/]*)/([0-9]*)").match(branch_string)
 
-if result:
-    (_, number) = result.groups()
-    print("AZ-{}: ".format(number), end="")
-    '';
-  };
-)
+    if result:
+        (_, number) = result.groups()
+        print("AZ-{}: ".format(number), end="")
+  '';
+})
