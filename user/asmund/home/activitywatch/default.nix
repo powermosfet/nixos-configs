f5949c62c1a30a0257bf727f6434ba5b18038c0a
Asmund Berge <asmund@conta.no>
2025-06-27 11:11:18 +0200

    Activitywatch: Add AFK watcher

--- user/asmund/home/activitywatch/default.nix
+++ user/asmund/home/activitywatch/default.nix
@@ -3,5 +3,15 @@
 {
   services.activitywatch = {
     enable = true;
+
+    watchers = {
+      aw-watcher-afk = {
+        package = pkgs.activitywatch;
+        settings = {
+          timeout = 300;
+          poll_time = 2;
+        };
+      };
+    };
   };
 }
