500465867951981ed08f21a49b073e64c1d4af66
Asmund Berge <asmund@conta.no>
2025-06-28 10:23:41 +0200

    Activitywatch: Remove watcher config

--- user/asmund/home/activitywatch/default.nix
+++ user/asmund/home/activitywatch/default.nix
@@ -3,15 +3,5 @@
 {
   services.activitywatch = {
     enable = true;
-
-    watchers = {
-      aw-watcher-afk = {
-        package = pkgs.activitywatch;
-        settings = {
-          timeout = 300;
-          poll_time = 2;
-        };
-      };
-    };
   };
 }
