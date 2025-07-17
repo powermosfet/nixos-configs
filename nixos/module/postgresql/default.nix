a95372d72be67fc71dd12009847056ff72525f75
Asmund Berge <asmund@berge.id>
2025-07-17 13:02:52 +0200

    Revert "Postgresql: Attempt upgrade to v14"
    This reverts commit 1b9ed2ff0d32b8d5067b579979c171aa056a3dac.

--- nixos/module/postgresql/default.nix
+++ nixos/module/postgresql/default.nix
@@ -7,7 +7,7 @@
 
   services.postgresql = {
     enable = true;
-    package = pkgs.postgresql_14;
+    package = pkgs.postgresql_13;
 
     extensions = with pkgs.postgresql15Packages; [ pg_safeupdate ];
   };
