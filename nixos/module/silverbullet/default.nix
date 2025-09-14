e80249f9dc8acc36fad0a2b363ac74649b23e101
Asmund Berge <asmund@berge.id>
2025-09-14 11:27:02 +0200

    silverbullet: Remove authelia

--- nixos/module/silverbullet/default.nix
+++ nixos/module/silverbullet/default.nix
@@ -8,24 +8,9 @@
 let
   cfg = config.services.silverbullet;
   hostname = "sb.berge.id";
-  autheliaConfig = pkgs.writeText "authelia-config-silverbullet.yml" ''
-    # Authelia access control configuration
-    access_control:
-      default_policy: deny
-      rules:
-        - domain: ${hostname}
-          resources:
-            - '/.client/manifest.json$'
-            - '/.client/[a-zA-Z0-9_-]+.png$'
-            - '/service_worker.js$'
-          policy: bypass
-        - domain: ${hostname}
-          policy: one_factor
-  '';
 in
 {
   imports = [
-    ../authelia/silverbullet
   ];
 
   config = {
@@ -39,28 +24,14 @@ in
       virtualHosts."${hostname}" = {
         enableACME = true;
         forceSSL = true;
-        extraConfig = ''
-          include ${../authelia/snippet/authelia-location.conf};
-          set $upstream http://${cfg.listenAddress}:${builtins.toString (cfg.listenPort)};
-        '';
         locations = {
           "/" = {
-            proxyPass = "$upstream";
+            proxyPass = "http://${cfg.listenAddress}:${builtins.toString (cfg.listenPort)}";
             proxyWebsockets = true;
-
-            extraConfig = ''
-              include ${../authelia/snippet/proxy.conf};
-              include ${../authelia/snippet/authelia-authrequest.conf};
-            '';
           };
         };
       };
     };
     services.ddclient.domains = [ hostname ];
-
-    services.authelia.instances.silverbullet = {
-      enable = true;
-      settingsFiles = [ autheliaConfig ];
-    };
   };
 }
