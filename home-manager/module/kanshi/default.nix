34a151f946bf64dbe6efb719d506c661b1b0fedf
Asmund Berge <asmund@conta.no>
2025-09-25 12:17:57 +0200

    Kanshi: specify transform for all monitors always

--- home-manager/module/kanshi/default.nix
+++ home-manager/module/kanshi/default.nix
@@ -23,14 +23,15 @@
             criteria = "Lenovo Group Limited LEN P27h-10 0x01010101";
             mode = "2560x1440";
             scale = 1.0;
-            transform = "normal";
             position = "0,0";
+            transform = "normal";
           }
           {
             criteria = "AU Optronics 0x1EAC Unknown";
             mode = "1920x1200";
             scale = 1.5;
             position = "-1280,0"; # 1920 / 1.5 = 1280.0
+            transform = "normal";
           }
         ];
       }
@@ -42,12 +43,14 @@
             mode = "2560x1440";
             scale = 1.0;
             position = "-2560,-840";
+            transform = "normal";
           }
           {
             criteria = "AU Optronics 0x1EAC Unknown";
             mode = "1920x1200";
             scale = 1.0;
             position = "0,0";
+            transform = "normal";
           }
           {
             criteria = "Lenovo Group Limited LEN T27h-20 VNA49K51";
@@ -76,18 +79,21 @@
             mode = "1920x1200";
             scale = 1.0;
             position = "0,0";
+            transform = "normal";
           }
           {
             criteria = "Samsung Electric Company S27E390 H4HH204663";
             mode = "1600x900";
             scale = 1.0;
             position = "1920,0";
+            transform = "normal";
           }
           {
             criteria = "AU Optronics 0x303E Unknown";
             mode = "1600x900";
             scale = 1.0;
             position = "3520,0";
+            transform = "normal";
           }
         ];
       }
