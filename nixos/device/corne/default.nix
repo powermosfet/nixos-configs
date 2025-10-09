2821564c23afe69476216ad21fee1005ff86ab73
Asmund Berge <asmund@conta.no>
2025-10-09 06:44:23 +0200

    Corne: Set udev rule

--- nixos/device/corne/default.nix
+++ nixos/device/corne/default.nix
@@ -4,4 +4,7 @@
   environment.systemPackages = with pkgs; [
     vial
   ];
+  services.udev.extraRules = ''
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="4653", ATTRS{idProduct}=="0004", TAG+="uaccess", MODE:="0666"
+  '';
 }
