59a0f98971afbc67d79a7fd84370d0075883608d
Asmund Berge <asmund@berge.id>
2025-10-14 19:09:47 +0200

    Monitoring: Enable prometheus

---
+++ nixos/module/monitoring/default.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+{
+  services.prometheus.exporters.node = {
+    enable = true;
+    port = 9000;
+    enabledCollectors = [ "systemd" ];
+    extraFlags = [
+      "--collector.ethtool"
+      "--collector.softirqs"
+      "--collector.tcpstat"
+    ];
+  };
+}
