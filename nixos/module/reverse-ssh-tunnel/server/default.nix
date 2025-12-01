93bdb2c0a2ab34ca14c2f6dc418ca3730e0df1fe
Asmund Berge <asmund@berge.id>
2025-12-01 20:52:14 +0100

    SSH Tunnel: Add correct public key to server

--- nixos/module/reverse-ssh-tunnel/server/default.nix
+++ nixos/module/reverse-ssh-tunnel/server/default.nix
@@ -2,7 +2,7 @@
 
 let
   common = import ../common.nix;
-  public-key = "ssh-ed25519 AAAA...your-backup-node-public-key";
+  public-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPTfiLf2X+5tUzFSAn4C+2xQhB5RsRxNeqiqNGFvwG5i tunnel-client@agent25";
 in
 {
   users.users.tunnel = {
