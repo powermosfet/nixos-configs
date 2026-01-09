bdfaeace64d113aba939aad01dacb3e23f9bd007
Asmund Berge <asmund@conta.no>
2026-01-09 15:53:16 +0100

    Screenshot: Set some swappy options

--- home-manager/module/screenshot/default.nix
+++ home-manager/module/screenshot/default.nix
@@ -1,13 +1,28 @@
 { config, pkgs, ... }:
 
+let
+  dir = "${config.home.homeDirectory}/skjermbilder";
+in
 {
   home.packages = with pkgs; [
     grimblast
-    swappy
   ];
 
   home.sessionVariables = {
     GRIMBLAST_EDITOR = "swappy -f";
-    XDG_SCREENSHOTS_DIR = "${config.home.homeDirectory}/skjermbilder";
+    XDG_SCREENSHOTS_DIR = dir;
+  };
+
+  programs.swappy = {
+    enable = true;
+    settings = {
+      Default = {
+        auto_save = true;
+        line_size = 3;
+        paint_mode = "rectangle";
+        save_dir = dir;
+        save_filename_format = "%Y%m%d-%H%M%S-swappy.png";
+      };
+    };
   };
 }
