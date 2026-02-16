bad76566f5dee81d2ea553ed2d36bd3e92415f69
Asmund Berge <asmund@conta.no>
2026-02-16 11:13:57 +0100

    Borg: Swith agent25 to tailscale DNS name

--- nixos/module/borg/jobs/agent25/default.nix
+++ nixos/module/borg/jobs/agent25/default.nix
@@ -9,12 +9,16 @@ with lib;
 with builtins;
 
 {
+  imports = [
+    ../../../tailscale
+  ];
+
   config = {
     services.borgbackup.jobs."agent25" = {
       paths = config.backup.paths;
       encryption.mode = "keyfile";
       environment.BORG_RSH = "ssh -i /root/.ssh/id_borg-main-agent25";
-      repo = "ssh://borg@agent25.berge.id:222/./";
+      repo = "ssh://borg@agent25/./";
       compression = "auto,zstd";
       startAt = "04:00";
     };
