a47a09ff1fecea0883244ad6b9c080bb3a19d2c2
Asmund Berge <asmund.berge@itera.com>
2026-04-15 12:06:08 +0200

    Iteramac: Add a bunch of programs

--- home-manager/user/asmund/iteramac/default.nix
+++ home-manager/user/asmund/iteramac/default.nix
@@ -8,5 +8,25 @@
     ../../../module/direnv
   ];
 
+  nixpkgs.config.allowUnfree = true;
+
+  home.packages = with pkgs; [
+    entr
+    logseq
+    octave
+    pnpm
+    caddy
+  ];
+
   programs.keepassxc.enable = true;
+  programs.claude-code.enable = true;
+  programs.docker-cli.enable = true;
+  programs.gh.enable = true;
+  programs.ghostty = {
+    enable = true;
+    package = pkgs.ghostty-bin;
+  };
+  programs.lazygit.enable = true;
+  programs.visidata.enable = true;
+  programs.zellij.enable = true;
 }
