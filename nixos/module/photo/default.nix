56798e04ca535875dba91ade1524c4ff4b138b53
Asmund Berge <asmund@conta.no>
2026-06-08 18:27:02 +0200

    Remove lensfun overlay

--- nixos/module/photo/default.nix
+++ nixos/module/photo/default.nix
@@ -1,17 +1,8 @@
 { pkgs, pkgsUnstable, ... }:
 
-let
-  pkgsUnstableWithOverlay = pkgsUnstable.extend (import ./lensfun-overlay.nix);
-in
 {
-  imports = [
-    ../../device/canon-eos-350d
-  ];
-
-  nixpkgs.overlays = [ (import ./lensfun-overlay.nix) ];
-
   environment.systemPackages = with pkgs; [
-    pkgsUnstableWithOverlay.darktable
+    pkgsUnstable.darktable
     gimp
     hugin
   ];
